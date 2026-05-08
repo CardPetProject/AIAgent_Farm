794e499bd7d6246c71f73cdf6827fadbd32e1a3e
seungki <rlatmdrl4512@daum.net>
2026-05-08 17:20:24 +0900

    <feat> 스크린샷 기능 추가
    <feat> 스크린샷 기능 추가

--- Assets/Scripts/System/ScreenCaptureButton.cs
+++ Assets/Scripts/System/ScreenCaptureButton.cs
@@ -1,16 +1,41 @@
+using System;
+using System.IO;
 using UnityEngine;
 
 public class ScreenCaptureButton : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private string filePrefix = "farm_screenshot";
+
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.RightBracket))
+        {
+            CaptureScreenToDesktop();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void CaptureScreenToDesktop()
     {
-        
+        string saveDirectory = GetDesktopPath();
+        string fileName = $"{filePrefix}_{DateTime.Now:yyyyMMdd_HHmmss}.png";
+        string filePath = Path.Combine(saveDirectory, fileName);
+
+        ScreenCapture.CaptureScreenshot(filePath);
+        Debug.Log($"[ScreenCaptureButton] Screenshot saved: {filePath}", this);
+    }
+
+    private string GetDesktopPath()
+    {
+        string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+
+        if (!string.IsNullOrEmpty(desktopPath) && Directory.Exists(desktopPath))
+        {
+            return desktopPath;
+        }
+
+        Debug.LogWarning(
+            $"[ScreenCaptureButton] Desktop path is unavailable. Using persistentDataPath: {Application.persistentDataPath}",
+            this);
+        return Application.persistentDataPath;
     }
 }
